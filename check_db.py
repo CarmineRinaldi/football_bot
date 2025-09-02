f42a3847a24863a15f994b4e435d61deaa04a1df
CarmineRinaldi <carminerinaldi93@gmail.com>
2025-09-02 21:24:54 +0200

    check_db.py

--- check_db.py
+++ check_db.py
@@ -1,14 +1,5 @@
-import sqlite3
-import os
+# check_db.py
+from db import get_all_users
 
-DB_FILE = os.environ.get("DB_FILE", "users.db")
-
-conn = sqlite3.connect(DB_FILE)
-c = conn.cursor()
-c.execute("SELECT * FROM users")
-rows = c.fetchall()
-conn.close()
-
-print("Utenti nel DB:")
-for r in rows:
-    print(r)
+users = get_all_users()
+print("Utenti nel DB:", users)
