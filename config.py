538f750e51aabc528cb12a8b51f61f855179a8f1
CarmineRinaldi <carminerinaldi93@gmail.com>
2025-09-03 21:32:00 +0200

    Delete config.py

--- config.py
+++
@@ -1,6 +0,0 @@
-TOKEN = "8488759682:AAEdaxTctJ1Yy21fIhXqIl9Y3IJf2VdJnC8"
-STRIPE_API_KEY = "sk_test_51S2oY0Jgfcb9GRQcfD9nmylH6KBGtbeCOFaLyGbwmh03dwi81Pqcz7z9rduFB5tSWS2ZpJJ2v2RPZB8c5NyGx57o006CNwLc5O"
-
-# Prezzi in centesimi (Stripe lavora in centesimi)
-PLAN_10_CREDITS_PRICE = 200  # 2 euro
-PLAN_VIP_PRICE = 500          # 5 euro
