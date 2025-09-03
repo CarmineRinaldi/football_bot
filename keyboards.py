0b66f9214cf0683e9fa7cd6fdfe1416e9fae091c
CarmineRinaldi <carminerinaldi93@gmail.com>
2025-09-03 23:42:22 +0200

    Delete keyboards.py

--- keyboards.py
+++
@@ -1,9 +0,0 @@
-from telegram import InlineKeyboardButton, InlineKeyboardMarkup
-
-def main_keyboard():
-    keyboard = [
-        [InlineKeyboardButton("Free pronostici", callback_data="free")],
-        [InlineKeyboardButton("Compra 10 pronostici (2€)", callback_data="buy_10")],
-        [InlineKeyboardButton("Abbonamento VIP 10 al giorno", callback_data="vip")],
-    ]
-    return InlineKeyboardMarkup(keyboard)
