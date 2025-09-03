5c2e25d35e06d2f3a9a449f686448982f4a0c961
CarmineRinaldi <carminerinaldi93@gmail.com>
2025-09-03 21:12:43 +0200

    Create config.py

---
+++ config.py
@@ -0,0 +1,6 @@
+TOKEN = "INSERISCI_IL_TUO_TOKEN"
+STRIPE_API_KEY = "INSERISCI_LA_TUA_CHIAVE_STRIPE"
+
+# Prezzi in centesimi (Stripe lavora in centesimi)
+PLAN_10_CREDITS_PRICE = 200  # 2 euro
+PLAN_VIP_PRICE = 500          # 5 euro
