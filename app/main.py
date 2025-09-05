78f4a1d540aea9c10b8a94b3647d5751fcf5c191
CarmineRinaldi <carminerinaldi93@gmail.com>
2025-09-05 09:09:40 +0200

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,18 +1,16 @@
 from fastapi import FastAPI, Request
-import asyncio
-from bot.bot import bot, dp
-from bot.queue_handler import UpdateQueue
+from aiogram import Bot, Dispatcher, types
+import os
 
 app = FastAPI()
+
+TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN")
+bot = Bot(token=TG_BOT_TOKEN)
+dp = Dispatcher(bot)
 
 @app.post("/webhook")
 async def telegram_webhook(request: Request):
     data = await request.json()
-    # Metti l'update nella coda asincrona
-    await UpdateQueue.queue.put(data)
+    update = types.Update(**data)
+    await dp.process_update(update)
     return {"ok": True}
-
-# Background task per processare la coda
-@app.on_event("startup")
-async def start_background_tasks():
-    asyncio.create_task(UpdateQueue.process_queue(dp))
