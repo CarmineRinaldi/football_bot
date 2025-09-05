9956f17d22171c4ab7ea05bc050bb074008b9314
CarmineRinaldi <carminerinaldi93@gmail.com>
2025-09-05 16:58:16 +0200

    Delete app directory

--- app/utils/logger.py
+++
@@ -1,9 +0,0 @@
-import logging
-
-logger = logging.getLogger("football_bot")
-logger.setLevel(logging.INFO)
-handler = logging.StreamHandler()
-formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
-handler.setFormatter(formatter)
-if not logger.handlers:
-    logger.addHandler(handler)
