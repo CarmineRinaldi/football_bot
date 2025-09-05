bb3425d5be741a288d6e796f844546288bc9028f
CarmineRinaldi <carminerinaldi93@gmail.com>
2025-09-05 20:33:06 +0200

    Create footballAPI.js

---
+++ footballAPI.js
@@ -0,0 +1,17 @@
+const axios = require('axios');
+const API_KEY = process.env.API_FOOTBALL_KEY;
+
+async function getMatches(league) {
+  const res = await axios.get('https://v3.football.api-sports.io/fixtures', {
+    params: { league, season: 2025 },
+    headers: { 'x-apisports-key': API_KEY }
+  });
+  return res.data.response.map(f => ({
+    id: f.fixture.id,
+    home: f.teams.home.name,
+    away: f.teams.away.name,
+    odds: f.goals ? f.goals : 1.5
+  }));
+}
+
+module.exports = { getMatches };
