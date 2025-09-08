89392ca5aed52aff790766b9ce215836ddbddd93
CarmineRinaldi <carminerinaldi93@gmail.com>
2025-09-08 07:52:36 +0200

    Update start.py

--- handlers/start.py
+++ handlers/start.py
@@ -1,7 +1,6 @@
 from aiogram import types
 from aiogram.filters import Command
 from buttons import main_menu  # import assoluto
-import logging
 
 async def cmd_start(message: types.Message):
     await message.answer("Benvenuto al bot!", reply_markup=main_menu)
