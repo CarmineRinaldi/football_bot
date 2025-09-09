4ded4a461f8e32c8c84c451c01470e649d621ad1
CarmineRinaldi <carminerinaldi93@gmail.com>
2025-09-09 09:09:33 +0200

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,14 +1,13 @@
 from datetime import datetime, timedelta
-from config import FREE_MAX_MATCHES
 
-async def can_use_free(last_free, free_max=FREE_MAX_MATCHES):
+def can_use_free(last_free, free_max=5):
     if not last_free:
         return True
     delta = datetime.now() - last_free
     return delta >= timedelta(hours=24)
 
-async def format_schedine(schedine):
+def format_schedine(schedine):
     return "\n".join(f"- {s}" for s in schedine)
 
-async def waiting_message():
+def waiting_message():
     return "⏳ Attendere, sto preparando i dati..."
