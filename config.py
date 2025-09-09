025a5ad6b0fdd81b9631020be295e1aba9463c8e
CarmineRinaldi <carminerinaldi93@gmail.com>
2025-09-09 09:08:34 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,20 +1,12 @@
 import os
 
-# Telegram
+FREE_MAX_MATCHES = int(os.getenv("FREE_MAX_MATCHES", 5))
+VIP_MAX_MATCHES = int(os.getenv("VIP_MAX_MATCHES", 20))
+DATABASE_URL = os.getenv("DATABASE_URL", "users.db")
+API_FOOTBALL_KEY = os.getenv("API_FOOTBALL_KEY")
 TG_BOT_TOKEN = os.getenv("TG_BOT_TOKEN")
 WEBHOOK_URL = os.getenv("WEBHOOK_URL")
 
-# Database
-DATABASE_URL = os.getenv("DATABASE_URL", "users.db")
-
-# API Football
-API_FOOTBALL_KEY = os.getenv("API_FOOTBALL_KEY")
-
-# Free/VIP
-FREE_MAX_MATCHES = int(os.getenv("FREE_MAX_MATCHES", 5))
-VIP_MAX_MATCHES = int(os.getenv("VIP_MAX_MATCHES", 20))
-
-# Stripe
 STRIPE_SECRET_KEY = os.getenv("STRIPE_SECRET_KEY")
 STRIPE_ENDPOINT_SECRET = os.getenv("STRIPE_ENDPOINT_SECRET")
 STRIPE_PRICE_2EUR = os.getenv("STRIPE_PRICE_2EUR")
